c1d73bc931a7a9b0d22d92dfc4ccb7367ace9ed5
Nattawut Chamcheun <nattawutchamcheun@gmail.com>
2023-03-10 12:01:21 +0700

    package lesson

--- demo.js
+++ demo.js
@@ -4,8 +4,8 @@ console.log(hello);
 
 // let
 if (true) {
-    let world = "Hello World"; // block scoped
-    console.log(world);
+  let world = "Hello World"; // block scoped
+  console.log(world);
 }
 
 // const
